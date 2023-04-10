b6fd47ee9fe62f26ba244574233b68b5dc94c50b
Raúl Camacho <raulj.camacho@gmail.com>
2023-04-10 18:03:04 -0500

    🚀 feat(ArrayFilter.js): añadir filtro para eliminar elementos nulos en el array de frutas 🚀 feat(MethodFrom.js): añadir ejemplos de uso del método Array.from()

---
+++ js/arrays/ArrayFilter.js
@@ -0,0 +1,7 @@
+let frutas = ['Banana', 'Mango', 'Manzana', null, null, 'Pera'];
+
+frutas = frutas.filter(fruta => {
+  return fruta;
+});
+
+console.log(frutas);
--- js/arrays/MethodFrom.js
+++ js/arrays/MethodFrom.js
@@ -1,4 +1,27 @@
-function addOne() {
+
+
+
+/* Método Array.from crea una nueva instancia de un 
+arreglo a partir de un objeto iterable */
+
+console.log(Array.from('javascript'));
+
+/*   El método 'Array.from()' acepta una función de devolución de llamada que le permite 
+  ejecutar la función de mapeo en cada elemento de la matriz que se está creando. */
+
+function SumaUno() {
   return Array.from(arguments, x => x + 1);
 }
-console.log(addOne(1, 2, 3));
+console.log(SumaUno(1, 2, 3));
+
+
+
+
+
+
+
+function arrayFromArgs() {
+  return Array.from(arguments);
+}
+
+console.log(arrayFromArgs('javascript', 'node'));
